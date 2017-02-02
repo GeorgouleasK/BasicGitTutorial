04e39251fa3f46672e3cda8b3cbb3f81a7a20b99
Kyriakos Georgouleas <Georgouleas.Kiriakos@haicorp.com>
2017-02-02 11:13:51 +0200

    Test 1 added

--- BasicGitTutorial/src/example/com/GitTutorialMain.java
+++ BasicGitTutorial/src/example/com/GitTutorialMain.java
@@ -5,6 +5,9 @@ public class GitTutorialMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		// Test 1 added
+		System.out.println("Test 1");
 	}
 
 }
