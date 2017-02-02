babcb48692917474fb2e52ce989f240480d0bb4e
Kyriakos Georgouleas <Georgouleas.Kiriakos@haicorp.com>
2017-02-02 11:55:10 +0200

    Implementation of SalesData class

--- BasicGitTutorial/src/example/com/GitTutorialMain.java
+++ BasicGitTutorial/src/example/com/GitTutorialMain.java
@@ -6,6 +6,8 @@ public class GitTutorialMain {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		
+		SalesData sales = new SalesData();
+		sales.displayData();
 		// Test 1 added
 		System.out.println("Test 1");
 	}
