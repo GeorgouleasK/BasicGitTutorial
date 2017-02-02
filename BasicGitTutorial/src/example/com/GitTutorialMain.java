746f1c039e83793ce1a824a59dccd88a87e95a8e
Kyriakos Georgouleas <Georgouleas.Kiriakos@haicorp.com>
2017-02-02 12:12:27 +0200

    Display message for sales added

--- BasicGitTutorial/src/example/com/GitTutorialMain.java
+++ BasicGitTutorial/src/example/com/GitTutorialMain.java
@@ -5,6 +5,7 @@ public class GitTutorialMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HELLO WORLD");
+		System.out.println("DISPLAY SALES DATA");
 		
 		// Test 1 added
 		System.out.println("TEST 1");
